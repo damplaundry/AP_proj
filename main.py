9148cc2ff4dd6f72ab64aad8b1b5f7af62f83022
damplaundry <144713235+damplaundry@users.noreply.github.com>
2025-04-24 17:30:32 +0000

    working

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
 
-
-print("Hello! This is hangman, guess letters to solve the five letter word. If you get it wrong, you will lose one of ten lives.")
-
+print("Hello! This is hangman, guess letters to solve the word. If you get it wrong, you will lose one of ten lives.")
 
 def word():
     words = ["table", "happy", "classroom", "sphynx", "alternator"]
@@ -14,39 +12,47 @@ def display_word(word, guess):
         if letter in guess:
             displayed_word += letter + " "
         else:
-            displayed_word += "_"
+            displayed_word += "_ "
     return displayed_word.strip()
 
 while True:
     guessing_word = word()
     lives = 10
-    guesses = 0
     guessed = set()
     word_letters = set(guessing_word)
-    alphabet = set(chr(i) for i in range(ord("a"), ord("z") + 1))
-
-    while lives > 0 and len(word_letters):
-        print("Lives = " + str(lives))
-
+    tries = 0
+    
+    while lives > 0 and word_letters:  # Continue until we either run out of lives or guess the word
+        print("\nLives = " + str(lives))
+        print("Word to guess: " + display_word(guessing_word, guessed))  # Show word with blanks
         guess = input("Guess a letter: ").lower()
-        if guess in alphabet - guessed:
-            guessed.add(guess)
-            if guess in word_letters:
-                word_letters.remove(guessed)
-                print(" ")
-            else:
-                lives = lives - 1
-                guesses = guesses + 1
-                print("That letter is not in the word")
-
-        elif guess in guessed:
-            print("You have already guessed that letter")
+        tries = tries + 1
         
+        # Validate the input
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter only a single letter.")
+            continue
+        
+        if guess in guessed:
+            print("You have already guessed that letter")
+        elif guess in word_letters:
+            guessed.add(guess)
+            word_letters.remove(guess)
+            print("Good guess!")
         else:
-            print("Sorry, I don't know that character. Pleased try again.")
+            lives -= 1
+            guessed.add(guess)
+            print("That letter is not in the word.")
+        
+        if not word_letters:
+            print("\nHurray!!! You guessed the word: " + guessing_word + " correctly!")
+            print("\nIt  took  you " + str(tries)  + " tries to guess " + guessing_word)
+            break
     
     if lives == 0:
-        print("I'm sorry, you're out of lives")
-        print("You're word was: " + word)
-    else: 
-        print("Hurray!!! You guessed " + word + "correctly!")
+        print("\nI'm sorry, you're out of lives. The word was: " + guessing_word)
+    
+    play_again = input("\nWould you like to play again? (y/n): ").lower()
+    if play_again != 'y':
+        print("Thanks for playing!")
+        break
